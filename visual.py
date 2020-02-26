be4caa7941fccff3cb46bf64d40ed086b48e45a2
OrixAY <orix.auyeung@hunt.social>
2020-02-26 11:50:43 +0800

    add flask

--- visual.py
+++ visual.py
@@ -1,11 +1,12 @@
 import json
+import os
 
 import numpy as np
 import cv2
 
-test_annotation_file = "./data/annotations/(2)A2766008.1.json"
-test_image_file = "./data/images/(2)A2766008.1.png"
-
+test_annotation_file = "./data/annotations/(10)A2766020.1.json"
+test_image_file = "./data/images/(10)A2766020.1.png"
+output_path = "./data/annotated_images/"
 
 def get_annotations(annot_file):
     with open(annot_file, 'r') as f:
@@ -13,21 +14,23 @@ def get_annotations(annot_file):
     return annotations
 
 
-def draw():
-    image = cv2.imread(test_image_file)
-    annotations = get_annotations(test_annotation_file)
+def draw(image_file, annotation_file):
+    image = cv2.imread(image_file)
+    annotations = get_annotations(annotation_file)
     for annotation in annotations:
         vertices = np.array(annotation['bounding_box'])
         display_text = annotation['text']
         check_text = display_text.upper()
         if "DISPLAYSURFACE" in check_text or "SIGNBOARD" in check_text or "SIGNAGE" in check_text:
-            cv2.polylines(image, [vertices], 1, (0, 255, 0), 3)
+            cv2.polylines(image, [vertices], 1, (0, 0, 255), 3)
             cv2.putText(image, display_text,
-                        tuple(vertices[0]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=2)
-    winname = "test"
-    cv2.imshow(winname, image)
-    cv2.waitKey()
-    cv2.destroyWindow(winname)
+                        tuple(vertices[0]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), thickness=2)
+    cv2.imwrite(os.path.join(output_path, os.path.basename(image_file)), image)
 
 if __name__ == "__main__":
-    draw()
+    image_folder = "./data/images"
+    annotation_folder = "./data/annotations"
+    images = [os.path.join(image_folder, x) for x in os.listdir(image_folder)]
+    annotations = [os.path.join(annotation_folder, x) for x in os.listdir(annotation_folder)]
+    for i in range(len(images)):
+        draw(images[i], annotations[i])
